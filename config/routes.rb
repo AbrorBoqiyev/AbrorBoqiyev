72111a984cbe816d2bda05d8e1fe15ce760ea74b
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-10-18 10:55:24 +0500

    controllers/users and some fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    retistrations: 'users/retistrations',
+  }
   resources :posts
   get 'about', to: "pages#about"
-  root "pages#home"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
